ade9ae1ab932a4c832fd44e5afe2056c1c65adc5
Ben Pilgrim <ben@pilgrim.me.uk>
2020-06-14 10:51:06 +0100

    Readd message param

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ const path = require("path");
                             end_line: 0,
                             annotation_level: 'failure',
                             title: `JUnit Test "${testcase.name}" failed.`,
+                            message: `Test ${testcase.name} failed with ${error.type}`,
                             raw_details: testcase['system-out'] + '\n\n\n' + error['$t'],
                           });
                     }
